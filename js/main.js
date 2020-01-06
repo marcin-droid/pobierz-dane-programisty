d5daf85af9216db9674d63ff08132a5b9821dc74
Marcin Koziol <marcin_koziol@outlook.com>
2020-01-06 20:16:55 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-
     $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-        $('body').append('<div id="dane-programisty"></div>');
         $('#getData').click(function () {
+            $('body').append('<div id="dane-programisty"></div>');
             $('#dane-programisty').append('<p>Imię i nazwisko: ' + data.imie + ' ' + data.nazwisko + '</p>');
             $('#dane-programisty').append('<p>' + data.zawod + ' w firmie: ' + data.firma + '</p>');
         })
